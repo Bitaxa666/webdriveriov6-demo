b12d136cc173e1d9e7e281a5d5e7f7393d60a532
Vitaliy QA <pukich19@gmail.com>
2020-04-22 18:21:04 +0300

    Added more cases to the smoke specs

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -10,26 +10,63 @@ export class MainPagePO extends BasePO {
     this.pageSelectors = {
       h4header: "[data-testid='todo']",
       inputTitle: "[title='Title']",
-      inputField: "input[type='text']"
+      inputField: "input[type='text']",
+      inputDate: ".ant-picker-input input",
+      todayBtn: "a.ant-picker-today-btn",
+      sendBtn: "button[type='Submit']",
+      //TODO: need update selectors
+      firstComplete: "tr.ant-table-row-level-0:first-child a[href='#complete']",
+      firstTableItem: "tr.ant-table-row-level-0:first-child .ant-typography",
+      lastTableItem: "tr.ant-table-row-level-0:last-child .ant-typography"
     }
   }
 
-  get h4header() { return $(this.pageSelectors.h4header); }
+  get h4header() { return $(this.pageSelectors.h4header) }
 
-  get inputTitle() { return $(this.pageSelectors.inputTitle); }
+  get inputTitle() { return $(this.pageSelectors.inputTitle) }
 
-  get inputField() { return $(this.pageSelectors.inputField); }
+  get inputField() { return $(this.pageSelectors.inputField) }
 
-  open() { super.open('/'); }
+  get inputDate() { return $(this.pageSelectors.inputDate) }
 
-  inputTodo (todoText: string, testPause?: number):void {
-    this.inputField.waitForDisplayed({timeout: 10000, reverse: false, timeoutMsg: "Input modal isn't displayed"});
-    this.inputField.setValue(todoText);
+  get sendBtn() { return $(this.pageSelectors.sendBtn) }
+
+  get setTodayBtn() { return $(this.pageSelectors.todayBtn) }  
+
+  get firstTableItemTitle() { return $$(this.pageSelectors.firstTableItem)[0] }
+
+  get firstTableItemDate() { return $$(this.pageSelectors.firstTableItem)[1] }
+  
+  get firstCompleteLink() { return $(this.pageSelectors.firstComplete) }
+
+  get lastTableItemTitle() { return $$(this.pageSelectors.lastTableItem)[0] }
+
+  get lastTableItemDate() { return $$(this.pageSelectors.lastTableItem)[1] }
+
+  open() { super.open('/') }
+
+  fillInForm(todoText: string, todoData: string, testPause?: number): void {
+    this.inputField.waitForDisplayed({ timeout: 10000, reverse: false, timeoutMsg: "Input field isn't displayed" })
+    this.inputField.setValue(todoText)
+    this.inputDate.waitForDisplayed({ timeout: 10000, reverse: false, timeoutMsg: "Date field isn't displayed" })
+    this.inputDate.click()
+    this.setTodayBtn.waitForDisplayed({ timeout: 10000, reverse: false, timeoutMsg: "Today btn isn't displayed" })
+    this.setTodayBtn.click({button: 0})
     if (!testPause) {
-      browser.pause(500);
+      browser.pause(300)
     }
-    browser.pause(testPause);
+    browser.pause(testPause)
+  }
+
+  sendForm(): void {
+    this.sendBtn.waitForDisplayed({ timeout: 10000, reverse: false, timeoutMsg: "Send btn isn't displayed" })
+    this.sendBtn.click()
+  }
+  //TODO: add random function for item
+  setAsComplete(): void {
+    this.firstCompleteLink.waitForDisplayed({ timeout: 10000, reverse: false, timeoutMsg: "CompleteLink btn isn't displayed" })
+    this.firstCompleteLink.click()
   }
 }
 
-export const MainPage = new MainPagePO();
+export const MainPage = new MainPagePO()
--- tests/specs/smoke-tests/demo-1.spec.ts
+++ tests/specs/smoke-tests/demo-1.spec.ts
@@ -2,19 +2,37 @@ import {MainPage} from "../../pages/MainPage"
 
 describe('Add TODO item: Main page', function () {
     beforeEach(function () {
-        MainPage.open();
+        MainPage.open()
     })
+
     describe('1.[Main suite: page H4 Header]', function () {
         it('The H4 header should contain the "Add TODO item" text ', function () {
-            expect(MainPage.h4header).toHaveText("Add TODO item11");
+            expect(MainPage.h4header).toHaveText("Add TODO item")
         })
         it("Input field contains title: 'Title'", () => {
             const elem = MainPage.inputTitle
             expect(elem).toBeVisible()
-            expect(elem).toHaveText("Title");
+            expect(elem).toHaveText("Title")
         });
         it("Input field is available", () => {
             expect(MainPage.inputField).toBeClickable()
         });
+    })
+
+    describe('2.[Main suite: User can create new TODO]', function () {
+        it('After TODO creation new item added to the List', function () {
+            const itemText = "Test more than 5 symbols"
+            MainPage.fillInForm(itemText, "2020-04-01")
+            browser.pause(1000)
+            MainPage.sendForm()
+            expect(MainPage.lastTableItemTitle).toHaveText(itemText)
+        })
+    })
+    
+    describe('3.[Main suite: User can complete exist TODO]', function () {
+        it('After pressing the Complete btn - "true" class has been added', function () {
+            MainPage.setAsComplete()
+            expect(MainPage.firstTableItemTitle).toHaveClassContaining(' true')
+        })
     })
 })
